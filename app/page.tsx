376319cbbd6315985114a8a891426b1cde819a6f
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-10-28 20:12:38 -0400

    complete mobile styling

--- app/page.tsx
+++ app/page.tsx
@@ -22,53 +22,103 @@ export default function Home() {
         </div>
         <div className="flex flex-col gap-4 bg-rose-50 p-6 rounded-xl">
           <h3 className="text-rose-800 text-preset-3">Preparation time</h3>
-          <ul className="text-stone-600 text-preset-4">
-            <li>Total: Approximately 10 minutes</li>
-            <li>Preparation: 5 minutes</li>
-            <li>Cooking: 5 minutes</li>
+          <ul className="space-y-2 text-stone-600 text-preset-4">
+            <li className="flex gap-7 items-center ml-2">
+              <span className="text-rose-800 font-bold">•</span>
+              <span>
+                <strong>Total</strong>: Approximately 10 minutes
+              </span>
+            </li>
+            <li className="flex gap-7 items-center ml-2">
+              <span className="text-rose-800 font-bold">•</span>
+              <span>
+                <strong>Preparation</strong>: 5 minutes
+              </span>
+            </li>
+            <li className="flex gap-7 items-center ml-2">
+              <span className="text-rose-800 font-bold">•</span>
+              <span>
+                <strong>Cooking</strong>: 5 minutes
+              </span>
+            </li>
           </ul>
         </div>
         <div className="flex flex-col gap-6">
           <h2 className="text-preset-2 text-brown-800">Ingredients</h2>
-          <ul className="text-stone-600 text-preset-4">
-            <li>2-3 large eggs</li>
-            <li>Salt, to taste</li>
-            <li>Pepper, to taste</li>
-            <li>1 tablespoon of butter or oil</li>
-            <li>
-              Optional fillings: cheese, diced vegetables, cooked meats, herbs
+          <ul className="space-y-2 text-stone-600 text-preset-4">
+            <li className="flex gap-7 items-center ml-2">
+              <span className="text-brown-800 font-bold">•</span>
+              <span>2-3 large eggs</span>
+            </li>
+            <li className="flex gap-7 items-center ml-2">
+              <span className="text-brown-800 font-bold">•</span>
+              <span>Salt, to taste</span>
+            </li>
+            <li className="flex gap-7 items-center ml-2">
+              <span className="text-brown-800 font-bold">•</span>
+              <span>Pepper, to taste</span>
+            </li>
+            <li className="flex gap-7 items-center ml-2">
+              <span className="text-brown-800 font-bold">•</span>
+              <span>1 tablespoon of butter or oil</span>
+            </li>
+            <li className="flex gap-7 items-center ml-2">
+              <span className="text-brown-800 font-bold">•</span>
+              <span>
+                Optional fillings: cheese, diced vegetables, cooked meats, herbs
+              </span>
             </li>
           </ul>
         </div>
         <hr className="text-stone-150" />
         <div className="flex flex-col gap-6">
           <h2 className="text-brown-800 text-preset-2">Instructions</h2>
-          <ol className="text-stone-600 text-preset-4">
-            <li>
-              Beat the eggs: In a bowl, beat the eggs with a pinch of salt and
-              pepper until they are well mixed. You can add a tablespoon of
-              water or milk for a fluffier texture.
+          <ol className="space-y-2 text-stone-600 text-preset-4">
+            <li className="flex gap-4 ml-2">
+              <span className="text-brown-800 font-bold w-4 shrink-0">1.</span>
+              <span>
+                <strong>Beat the eggs</strong>: In a bowl, beat the eggs with a
+                pinch of salt and pepper until they are well mixed. You can add
+                a tablespoon of water or milk for a fluffier texture.
+              </span>
             </li>
-            <li>
-              Heat the pan: Place a non-stick frying pan over medium heat and
-              add butter or oil.
+            <li className="flex gap-4 ml-2">
+              <span className="text-brown-800 font-bold w-4 shrink-0">2.</span>
+              <span>
+                <strong>Heat the pan</strong>: Place a non-stick frying pan over
+                medium heat and add butter or oil.
+              </span>
             </li>
-            <li>
-              Cook the omelette: Once the butter is melted and bubbling, pour in
-              the eggs. Tilt the pan to ensure the eggs evenly coat the surface.
+            <li className="flex gap-4 ml-2">
+              <span className="text-brown-800 font-bold w-4 shrink-0">3.</span>
+              <span>
+                <strong>Cook the omelette</strong>: Once the butter is melted
+                and bubbling, pour in the eggs. Tilt the pan to ensure the eggs
+                evenly coat the surface.
+              </span>
             </li>
-            <li>
-              Add fillings (optional): When the eggs begin to set at the edges
-              but are still slightly runny in the middle, sprinkle your chosen
-              fillings over one half of the omelette.
+            <li className="flex gap-4 ml-2">
+              <span className="text-brown-800 font-bold w-4 shrink-0">4.</span>
+              <span>
+                <strong>Add fillings (optional)</strong>: When the eggs begin to
+                set at the edges but are still slightly runny in the middle,
+                sprinkle your chosen fillings over one half of the omelette.
+              </span>
             </li>
-            <li>
-              Fold and serve: As the omelette continues to cook, carefully lift
-              one edge and fold it over the fillings. Let it cook for another
-              minute, then slide it onto a plate.
+            <li className="flex gap-4 ml-2">
+              <span className="text-brown-800 font-bold w-4 shrink-0">5.</span>
+              <span>
+                <strong>Fold and serve</strong>: As the omelette continues to
+                cook, carefully lift one edge and fold it over the fillings. Let
+                it cook for another minute, then slide it onto a plate.
+              </span>
             </li>
-            <li>
-              Enjoy: Serve hot, with additional salt and pepper if needed.
+            <li className="flex gap-4 ml-2">
+              <span className="text-brown-800 font-bold w-4 shrink-0">6.</span>
+              <span>
+                <strong>Enjoy</strong>: Serve hot, with additional salt and
+                pepper if needed.
+              </span>
             </li>
           </ol>
         </div>
@@ -79,12 +129,35 @@ export default function Home() {
             The table below shows nutritional values per serving without the
             additional fillings.
           </p>
-          <ul>
-            <li>Calories 277kcal</li>
-            <li>Carbs 0g</li>
-            <li>Protein 20g</li>
-            <li>Fat 22g</li>
-          </ul>
+          <div>
+            <div className="flex gap-4 mx-8">
+              <span className="flex-1 text-preset-4">Calories</span>{" "}
+              <span className="flex-1 text-preset-4-bold text-brown-800">
+                277kcal
+              </span>
+            </div>
+            <hr className="text-stone-150 my-3" />
+            <div className="flex gap-4 mx-8">
+              <span className="flex-1 text-preset-4">Carbs</span>{" "}
+              <span className="flex-1 text-preset-4-bold text-brown-800">
+                0g
+              </span>
+            </div>
+            <hr className="text-stone-150 my-3" />
+            <div className="flex gap-4 mx-8">
+              <span className="flex-1 text-preset-4">Protein</span>{" "}
+              <span className="flex-1 text-preset-4-bold text-brown-800">
+                20g
+              </span>
+            </div>
+            <hr className="text-stone-150 my-3" />
+            <div className="flex gap-4 mx-8">
+              <span className="flex-1 text-preset-4">Fat</span>{" "}
+              <span className="flex-1 text-preset-4-bold text-brown-800">
+                22g
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </article>
